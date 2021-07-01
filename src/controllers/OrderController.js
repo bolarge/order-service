a905c2e2b76cbb2617d6e0773692da494f880183
oahwin <oahwin@getcarbon.co>
2021-07-01 16:40:43 +0100

    implement service that returns order history

--- src/controllers/OrderController.js
+++ src/controllers/OrderController.js
@@ -21,7 +21,7 @@ module.exports = {
         return res.status(200).json(new ServiceResponse(true, 'Retrieved order successfully', result))
       })
       .catch((err) => {
-        return res.status(500).json(new ServiceResponse(false, err.message, null, [err]))
+        return res.status(400).json(new ServiceResponse(false, err.message, null, [err]))
       });
   },
 
@@ -37,6 +37,17 @@ module.exports = {
       .catch((err) => {
         return res.status(500).json(new ServiceResponse(false, err.message, null, [err]))
       });
-  }
+  },
+
+  getOrderHistory: function (req, res) {
+    const clientId = req.params.clientId;
+    return orderService.getOrderHistory(clientId)
+      .then((result) => {
+        return res.status(200).json(new ServiceResponse(true, 'Retrieved list of orders successfully', result))
+      })
+      .catch((err) => {
+        return res.status(400).json(new ServiceResponse(false, err.message, null, [err]))
+      });
+  },
 }
 
